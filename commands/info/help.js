05eea7bfbb01a3075679e2f9ba0ad030e6d813d2
muhammed2100 <60185482+muhammed2100@users.noreply.github.com>
2021-01-15 11:20:36 +0100

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -27,7 +27,7 @@ module.exports = {
       const commands = await client.commands;
 
       let emx = new MessageEmbed()
-        .setDescription("Ali Lion Bot")
+        .setDescription("M-BOT")
         .setColor("RANDOM")
         .setFooter(client.user.username, client.user.displayAvatarURL())
         .setThumbnail(client.user.displayAvatarURL());
