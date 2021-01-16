e85c5a8e90a5fc1b10935fa31f66b9840c2378d6
muhammed2100 <60185482+muhammed2100@users.noreply.github.com>
2021-01-16 10:48:33 +0100

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,57 +1,45 @@
-const { MessageEmbed } = require("discord.js")
-const db = require("quick.db")
+const db = require('../../models/warns')
+const { Message, MessageEmbed } = require('discord.js')
 
 module.exports = {
-  name: "warn",
-  category: "moderation",
-  usage: "warn <@mention> <reason>",
-  description: "Warn anyone who do not obey the rules",
-  run: async (client, message, args) => {
-    
-    if(!message.member.hasPermission("ADMINISTRATOR")) {
-      return message.channel.send("You should have admin perms to use this command!")
+    name :'warn',
+    category: "moderation",
+    /**
+     * @param {Message} message
+     */
+    run : async(client, message, args) => {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command.')
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        if(!user) return message.channel.send('User not found.')
+        const reason = args.slice(1).join(" ")
+        db.findOne({ guildid: message.guild.id, user: user.user.id}, async(err, data) => {
+            if(err) throw err;
+            if(!data) {
+                data = new db({
+                    guildid: message.guild.id,
+                    user : user.user.id,
+                    content : [
+                        {
+                            moderator : message.author.id,
+                            reason : reason
+                        }
+                    ]
+                })
+            } else {
+                const obj = {
+                    moderator: message.author.id,
+                    reason : reason
+                }
+                data.content.push(obj)
+            }
+            data.save()
+        });
+        user.send(new MessageEmbed()
+            .setDescription(`You have been warned for ${reason}`)
+            .setColor("RED")
+        )
+        message.channel.send(new MessageEmbed()
+            .setDescription(`Warned ${user} for ${reason}`).setColor('BLUE')
+        )
     }
-    
-    const user = message.mentions.members.first()
-    
-    if(!user) {
-      return message.channel.send("Please Mention the person to who you want to warn - warn @mention <reaosn>")
-    }
-    
-    if(message.mentions.users.first().bot) {
-      return message.channel.send("You can not warn bots")
-    }
-    
-    if(message.author.id === user.id) {
-      return message.channel.send("You can not warn yourself")
-    }
-    
-    if(user.id === message.guild.owner.id) {
-      return message.channel.send("You jerk, how you can warn server owner -_-")
-    }
-    
-    const reason = args.slice(1).join(" ")
-    
-    if(!reason) {
-      return message.channel.send("Please provide reason to warn - warn @mention <reason>")
-    }
-    
-    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
-    
-    if(warnings === 3) {
-      return message.channel.send(`${message.mentions.users.first().username} already reached his/her limit with 3 warnings`)
-    }
-    
-    if(warnings === null) {
-      db.set(`warnings_${message.guild.id}_${user.id}`, 1)
-      user.send(`You have been warned in **${message.guild.name}** for ${reason}`)
-      await message.channel.send(`You warned **${message.mentions.users.first().username}** for ${reason}`)
-    } else if(warnings !== null) {
-        db.add(`warnings_${message.guild.id}_${user.id}`, 1)
-       user.send(`You have been warned in **${message.guild.name}** for ${reason}`)
-      await message.channel.send(`You warned **${message.mentions.users.first().username}** for ${reason}`)
-    }
-    
-  
-  } 
 }
